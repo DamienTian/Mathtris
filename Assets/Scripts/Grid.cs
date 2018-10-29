448f2fcd6cfb9110d4394ee188c7bd6fedaa6222
Damien Tian <tian4damien@gmail.com>
2018-10-29 12:52:04 -0400

    Initial commit

---
+++ Assets/Scripts/Grid.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour {
+
+    public static int width = 10;
+    public static int height = 20;
+
+    // grid: tracks all position of the grid
+    public static Transform[,] grid = new Transform[width, height];
+
+    // RoundPosition:
+       /* 
+        During the rotation of a block, the position might not stay at an integer
+        coordinate, so we need this fucntion to keep all the coordinates in 
+        integer.
+       */
+    public static Vector2 RoundPosition(Vector2 position)
+    {
+        return new Vector2(Mathf.Round(position.x), Mathf.Round(position.y));
+    }
+
+    // InsideBoarder:
+    // check if the block is inside the boarders
+    public static bool InsideBoarder(Vector2 position)
+    {
+        return ((int)position.x >= 0 &&
+                (int)position.x < width &&
+                (int)position.y >= 0);
+    }
+
+    public static bool IsRowFull(int y)
+    {
+        for (int x = 0; x < width; ++x)
+        {
+            if(grid[x,y] == null)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void DeleteRow(int y)
+    {
+        for (int x = 0; x < width; x++)
+        {
+            Destroy(grid[x, y].gameObject);
+            grid[x, y] = null;
+        }
+    }
+
+    public static void DeleteFullRows()
+    {
+        for (int y = 0; y < height; ++y)
+        {
+            if(IsRowFull(y))
+            {
+                DeleteRow(y);
+                DecreaseRowsAbove(y + 1);
+                // reset the y after delete the ro in order to keep the index right
+                --y;
+            }
+        }
+    }
+
+    public static void DecreaseRow(int y)
+    {
+        for (int x = 0; x < width; ++x)
+            // why ++x?
+        {
+            if (grid[x, y] != null)
+            {
+                grid[x, y - 1] = grid[x, y]; // why not grid[x, y] = grid[x, y - 1]
+                grid[x, y] = null;
+
+                // Updata Block position
+                grid[x, y - 1].position += new Vector3(0, -1, 0);
+            }
+        }
+    }
+
+    // 
+    public static void DecreaseRowsAbove(int y)
+    {
+        for (int i = y; i < height; ++i)
+        {
+            DecreaseRow(i);
+        }
+    }
+
+
+
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+    // Different shapes of blocks
+    [SerializeField] public GameObject[] shapes;
+
+    // Spawn the next blocks into the game surface
+    public void SpawnNext() 
+    {
+        int randomIndex = Random.Range(0, shapes.Length);
+
+        Instantiate(shapes[randomIndex],
+                    transform.position,
+                    Quaternion.identity);
+    }
+
+    void Start()
+    {
+        SpawnNext();
+    }
+}
